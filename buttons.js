94ab91347d043328ccb3c09f60bb33b8e854a659
reddicka <yu.nailka.rav@yandex.ru>
2022-01-13 16:51:25 +0500

    wqw

--- buttons.js
+++ buttons.js
@@ -1,14 +1,17 @@
+// Для кнопки лайков
 let like = document.querySelector('.like');
 let likeCount = document.querySelector('.like-count');
 
+// Для кнопки ломания
 let lomalkaButton = document.querySelector('.lomalka-button');
 let wrapper = document.querySelector('.wrapper');
 
+// Для всего, что после ломания
 let body = document.querySelector('body');
-let lastText = document.createElement('p');
+let htmlCode = '<div class="bidlo"><h1>ты чево наделал сука(9((<div class="sad"><div class="rot"></div></div></div>';
 
+// Кнопка и счетчик лайков
 like.onclick = function () {
-    
     if (like.classList.contains('like--added')) {
         likeCount.textContent--;
     } else {
@@ -18,12 +21,9 @@ like.onclick = function () {
     like.classList.toggle('like--added');
 }
 
-
+// Ломание
 lomalkaButton.onclick = function () {
     lomalkaButton.classList.toggle('lomalka-button--added');
     wrapper.remove();
-
-    body.classList.add('body-fail');
-    lastText.textContent = 'ты чево наделал сука(9((';
-    body.appendChild(lastText);
+    body.insertAdjacentHTML('afterbegin', htmlCode);
 }
---
+++ table.js
@@ -0,0 +1,24 @@
+const inner = document.querySelector('.inner');
+const squareColors = ['#ed220d', '#b542e5', '#0d93ed', '#f2770b', '#8affbc', '#f2ff8a', '#ff38c2', '#e37f35'];
+const BLOCKS = 672;
+
+for (let i = 0; i < BLOCKS; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.addEventListener('mouseover', () => setColor(square));
+    square.addEventListener('mouseout', () => removeColor(square));
+    inner.appendChild(square)
+}
+
+function setColor(elem) {
+    const color = getRandomColor();
+    elem.style.background = color;
+    elem.style.boxShadow = `0 0 12px ${color}`;
+}
+
+function removeColor(elem) {
+    elem.style.background = '#2d2d2d';
+    elem.style.boxShadow = '0 0 4px #000';
+}
+
+const getRandomColor = () => squareColors[Math.floor(Math.random() * squareColors.length)];
