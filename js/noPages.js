d002e8572493a7014a7caa33fa682b9957847ec8
reddicka <yu.nailka.rav@yandex.ru>
2022-01-17 17:02:05 +0500

    r

--- js/noPages.js
+++ js/noPages.js
@@ -1,38 +1,10 @@
-//var link = document.querySelectorAll('.menu__list > li > a');
-var link = document.querySelectorAll('.menu__list > li > a > p');
+document.addEventListener("click", event => piska(event));
 
-//console.log(link[4]);
-//--console.log('Text: ' + link[0].innerText);
-
-/* link.onclick = function () {
-    if (link.innerText == '...') {
-        alert('В анус себе тыкни');
+function piska(event) {
+    if (event.target.innerText == "...") {
+        alert('В очько себе тыкни, даун');
     }
-} */
-
-/*--for (let elem of link) {
-    if (elem.matches('innerText="..."')) {
-        console.log('Text: ' + link[0].innerText);
-        //--alert("В анус себе тыкни");
-    }
-}--*/
-
-/*---for (let elem of link) {
-    if (elem.matches('link[3].innerText="..."')) {
-        console.log('Text: ' + link[0].innerText);
-        //--alert("В анус себе тыкни");
-    }
-}---*/
-
-//----link.forEach(link => console.log('Множество: ', link.querySelector('a').innerText));
-
-/*link.onclick = function(event) {
-    if (event.target.innerText == '...') {
-        console.log('7767678');
-    }
-            //--alert("В анус себе тыкни");
-}*/
-
+} 
 
 /*
 console.log('Link: ' + link.href);
